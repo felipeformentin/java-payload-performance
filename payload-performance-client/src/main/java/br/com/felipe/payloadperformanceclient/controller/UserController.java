208f323f4aff3355df620e9170d737b5157b2fd7
Felipe Formentin <claiohm@gmail.com>
2020-08-02 15:21:24 -0300

    Adding more threads/network calls

--- payload-performance-client/src/main/java/br/com/felipe/payloadperformanceclient/controller/UserController.java
+++ payload-performance-client/src/main/java/br/com/felipe/payloadperformanceclient/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class UserController {
@@ -16,12 +18,20 @@ public class UserController {
     private UserClient client;
 
     @GetMapping(path = "/v1/users")
-    List<User> getUsers() {
-        return client.getUsers();
+    List<User> getUsers() throws ExecutionException, InterruptedException {
+        CompletableFuture<List<User>> cf1 = CompletableFuture.supplyAsync(() -> client.getUsers());
+        CompletableFuture<Void> cf2 = CompletableFuture.runAsync(() -> client.getUsers());
+        CompletableFuture<Void> cf3 = CompletableFuture.runAsync(() -> client.getUsers());
+        CompletableFuture.allOf(cf1, cf2, cf3).get();
+        return cf1.get();
     }
 
     @GetMapping(path = "/v2/users")
-    List<UserV2> getUsersV2() {
-        return client.getUsersV2();
+    List<UserV2> getUsersV2() throws ExecutionException, InterruptedException {
+        CompletableFuture<List<UserV2>> cf1 = CompletableFuture.supplyAsync(() -> client.getUsersV2());
+        CompletableFuture<Void> cf2 = CompletableFuture.runAsync(() -> client.getUsersV2());
+        CompletableFuture<Void> cf3 = CompletableFuture.runAsync(() -> client.getUsersV2());
+        CompletableFuture.allOf(cf1, cf2, cf3).get();
+        return cf1.get();
     }
 }
